056ee8f74a126a8d99e02b0ba2eb334c35297769
nabilchabab <nabil.chababnabil@gmail.com>
2023-09-13 14:14:05 +0100

    challenge2

---
+++ C Challenges/challenge2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+
+int main(){
+
+
+    int C , F;
+
+    printf("Entrer la temperature en Fahrenheit :");
+    scanf("%d",&F);
+
+    C = (F-32)/1.8;
+    printf("La transforme en degre Celsius est : %d",C);
+
+    if (C>=40){
+        printf("Donc tres chaud!!!");
+    }
+    else if (C >= 30 && C < 40){
+        printf("\nDonc chaud!!!");
+    }
+    else if (C <= 30){
+        printf("\nDonc froid!!!");
+    }
+    else{
+        printf("\nDonc tres froid!!!");
+    }
+    
+
+
+
+    return 0 ;
+}
