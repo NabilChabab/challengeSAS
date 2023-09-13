dbddeb84f8e7791fc9f13050843ae43138ed8eb2
nabilchabab <nabil.chababnabil@gmail.com>
2023-09-13 19:22:03 +0100

    challenge8

---
+++ C Challenges/challenge8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main(){
+
+    int n;
+    printf("Donner un nombre : ");
+    scanf("%d",&n);
+
+    printf("la valeur de n en Octal est : %o\n",n);
+    printf("la valeur de n en Hexa est : %X",n);
+
+    
+    return 0;
+}
