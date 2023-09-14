eb220cae42c789f1b79f9232366ddabbfc71c656
nabilchabab <nabil.chababnabil@gmail.com>
2023-09-14 13:57:12 +0100

    Challenge

--- C Challenges/challenge11Condi.c
+++ C Challenges/challenge11Condi.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
 
 int main(){
 
+    srand(time(NULL));
+    
+    int num = ((rand() % 7) - 1);
+
+
+    switch (num)
+    {
+    case 1:
+        printf("lundi",num);
+        break;
+    case 2:
+        printf("mardi",num);
+        break;
+    case 3:
+        printf("mercredi",num);
+        break;
+    case 4:
+        printf("jeudi",num);
+        break;
+    case 5:
+        printf("vendredi",num);
+        break;
+    case 6:
+        printf("samedi",num);
+        break;                    
+    case 7:
+        printf("dimanche",num);
+        break;
+    
+    }
     
     return 0;
 }
