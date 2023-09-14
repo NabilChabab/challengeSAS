f6981ccd981a4683926954781d751d20dff59881
nabilchabab <nabil.chababnabil@gmail.com>
2023-09-14 10:06:16 +0100

    challenge9Condi

--- C Challenges/challenge9Condi.c
+++ C Challenges/challenge9Condi.c
@@ -9,10 +9,13 @@ int main(){
     scanf("%c",&alphabet);
 
     if ((alphabet >=65 && alphabet<=90)){
-        printf("c'est un alphabet Majuscule");
+        printf("c'est un alphabet et Majuscule");
+    }
+    else if ((alphabet >=97 && alphabet<=122)){
+        printf("c'est un alphabet et Miniscule");
     }
     else{
-        printf("c'est pas un alphabet Majuscule");
+        printf("c'est pas un alphabet");
     }
     
     
